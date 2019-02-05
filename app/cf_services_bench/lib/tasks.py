883e0422cf7b8c66e341eb222d5e216d475da5f0
Axel FAUVEL <afauvel.ext@orange.com>
2019-02-05 16:48:15 +0100

    factorisation

--- app/cf_services_bench/lib/tasks.py
+++ app/cf_services_bench/lib/tasks.py
@@ -3,6 +3,7 @@ import os
 from celery import Celery
 from .config import Config
 from .model import RedisWrapper
+from .results import upsert_result
 import json
 
 config = Config()
@@ -12,16 +13,6 @@ celery = Celery('tasks', broker=redis_uri)
 
 @celery.task
 def bench(service, service_instance, scenario, token):
-    session_result = {'service': service,
-                      'service_instance': service_instance,
-                      'scenario': scenario}
-    redis_conn = RedisWrapper(redis_uri)
-    key = '{}{}'.format(config.redis_key_prefix, token)
-
-    previous_result = redis_conn.get(key)
-    if previous_result:
-        result = json.loads(previous_result.decode('utf-8'))
-        result.append(session_result)
-    else:
-        result = [session_result]
-    redis_conn.set_kv(key, json.dumps(result))
+    result = 'blablabla'
+    upsert_result(config, service,
+                  service_instance['name'], scenario, token, result)
