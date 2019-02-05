9bba5379afa19478b0e15eae09780bfb6565952b
Axel FAUVEL <afauvel.ext@orange.com>
2019-02-05 16:11:57 +0100

    finishing api side

--- app/cf_services_bench/lib/tasks.py
+++ app/cf_services_bench/lib/tasks.py
@@ -2,7 +2,8 @@
 import os
 from celery import Celery
 from .config import Config
-
+from .model import RedisWrapper
+import json
 
 config = Config()
 redis_uri = config.get_redis_storage_uri()
@@ -10,7 +11,17 @@ celery = Celery('tasks', broker=redis_uri)
 
 
 @celery.task
-def bench(service, service_instance, scenario):
-    print('service: {}'.format(service))
-    print('service_instance: {}'.format(service_instance))
-    print('scenario: {}'.format(scenario))
+def bench(service, service_instance, scenario, token):
+    session_result = {'service': service,
+                      'service_instance': service_instance,
+                      'scenario': scenario}
+    redis_conn = RedisWrapper(redis_uri)
+    key = '{}{}'.format(config.redis_key_prefix, token)
+
+    previous_result = redis_conn.get(key)
+    if previous_result:
+        result = json.loads(previous_result.decode('utf-8'))
+        result.append(session_result)
+    else:
+        result = [session_result]
+    redis_conn.set_kv(key, json.dumps(result))
