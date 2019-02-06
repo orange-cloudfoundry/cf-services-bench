09db7fd3263a32c0c279233f6bf3d82f7e94c007
Axel FAUVEL <afauvel.ext@orange.com>
2019-02-06 11:12:28 +0100

    adding documentation

--- app/cf_services_bench/lib/results.py
+++ app/cf_services_bench/lib/results.py
@@ -6,6 +6,18 @@ from .model import RedisWrapper
 
 def upsert_result(config, service, service_instance_name, scenario, token,
                   result):
+    """Stores results in redis. Check if result previously exists and update
+    it if so
+
+    Arguments:
+
+        config {[object]} -- config object
+        service {[str]} -- [service that will be benched (redis, mysql...)]
+        service_instance {[dict]} -- [credentials extracted from VCAP_SERVICES]
+        scenario {[str]} -- [bench scenario to run can be nominal or benchmark]    
+        token {[str]} -- [unique token to identify bench session]
+        result {[str or dict]} -- result that will be stored in redis
+    """
 
     session_result = {'service': service,
                       'service_instance': service_instance_name,
@@ -25,5 +37,14 @@ def upsert_result(config, service, service_instance_name, scenario, token,
 
 
 def get_last_result_as_json(config):
+    """returns last result formated in JSON
+
+    Arguments:
+        config {[object]} -- config object
+
+    Returns:
+        [JSON] -- last result
+    """
+
     redis_conn = RedisWrapper(config.get_redis_storage_uri())
     return json.dumps(redis_conn.get_last_result(config.redis_key_prefix))
