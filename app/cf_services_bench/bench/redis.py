402cd0a869dc1ef30f5a63135dfeaa6be704eae6
Axel FAUVEL <afauvel.ext@orange.com>
2019-02-11 12:16:49 +0100

    adding DONT_USE_REDIS_BENCHMARK option for redis and use a custom script instead of redis-benchmark

--- app/cf_services_bench/bench/redis.py
+++ app/cf_services_bench/bench/redis.py
@@ -28,10 +28,18 @@ class BenchRedis(Bench):
         self.hostname, self.port = redis_uri.split("@")[1].split(":")
         self.password = redis_uri.split("@")[0].split("//")[1][1:]
 
-        if not os.environ.get("LOCAL", False):
-            self.cmd = sh.Command("/home/vcap/app/bin/redis-benchmark")
-        else:
+        local = os.environ.get("LOCAL", False)
+        use_redis_benchmark = not os.environ.get(
+            "DONT_USE_REDIS_BENCHMARK", False)
+
+        if local and use_redis_benchmark:
             self.cmd = sh.Command("/usr/bin/redis-benchmark")
+        elif local and not use_redis_benchmark:
+            self.cmd = sh.Command("/usr/bin/python")
+        elif not local and use_redis_benchmark:
+            self.cmd = sh.Command("/home/vcap/app/bin/redis-benchmark")
+        elif not local and not use_redis_benchmark:
+            self.cmd = sh.Command("/home/vcap/deps/0/python/bin/python")
 
         if scenario == "nominal":
             self.options = [
